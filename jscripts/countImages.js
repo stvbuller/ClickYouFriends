fab99f154f0fe636b49dd03e4f48f99b6751fb36
Steven Buller <steven@Stevens-MacBook-Pro.local>
2015-11-16 15:16:30 -0500

    finished

--- jscripts/countImages.js
+++ jscripts/countImages.js
@@ -12,29 +12,13 @@ for(var i = 0; i < imagesClassmates.length; i++) {
 
 function gameStarts() {
   if(startStopBtn.getAttribute("data-state") === "stop"){
+    if (this.getAttribute("data-state") === "notclicked") {
+      this.setAttribute("data-state", "clicked")
       imageCounter++;
-      alert("This is the " + imageCounter + " image you have clicked");
+    } else { 
+      alert("You have already clicked this image");
+   }  
   }
 }
 
-/* the idea is to to check the data-state of an image, if the
-    data-state is "notclicked" count the image and change the 
-    data-state to "clicked", if the data-state is "clicked" do
-    not count the image, at the end of the game the data-state 
-    for all images must be set to "notclicked"
-
-   if (this.getAttribute("data-state") === "notclicked") {
-      this.setAttribute("data-state", "clicked")
-      imageCounter++;
-      alert("This is the " + imageCounter + " image you have clicked");
-    } else { 
-      alert("You have already clicked this image");
-  } 
-
-for(var i = 0; i < imagesClassmates.length; i++) {
-  imagesClassmates[i].setAttribute("data-state", "notclicked");
-}
-
-
-  */
 
